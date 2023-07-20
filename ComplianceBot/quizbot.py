dcf8a6a8193e9df06264aec67b1ed1ba20988d57
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-20 17:35:44 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def load_data():
     # CSV 파일을 로드합니다.
-    data = pd.read_csv("your_csv_file.csv")
+    data = pd.read_csv("cocmp_quiz.csv")
     return data
 
 def get_question(sheet_name, question_number, data):
