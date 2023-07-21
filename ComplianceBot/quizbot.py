dbc829b24a67ec5fdb648994017b4bccba1f04a5
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-21 09:48:39 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def load_data():
-    data_url = "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/comp_quiz02.csv"
+    data_url = "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/comp_quiz.csv"
     data = pd.read_csv(data_url)
     return data
 
@@ -10,7 +10,7 @@ def main():
     data = load_data()
 
     st.title("Quiz App")
-    st.write("문항 번호를 입력하세요.")
+    st.write("문항 번호를 입력하여 문제를 풀어보세요.")
 
     question_number = st.text_input("문항 번호를 입력하세요.")
 
@@ -20,13 +20,14 @@ def main():
         st.write("문제:", question["문항"])
 
         # O, X 버튼 생성
-        user_answer = st.radio("정답을 선택하세요.", ("O", "X"))
+        user_answer = st.radio("정답을 선택하세요.", ("O", "X"), index=None)
 
-        if user_answer == question["답안"]:
-            st.write("정답입니다!")
-        else:
-            st.write("틀렸습니다.")
-            st.write("해설:", question["해설"])
+        if user_answer is not None:
+            if user_answer == question["답안"]:
+                st.write("정답입니다!")
+            else:
+                st.write("틀렸습니다.")
+                st.write("해설:", question["해설"])
 
 if __name__ == "__main__":
     main()
