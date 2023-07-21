4d0d2c4de04f6ddd057310f0409f89399c7db105
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-21 16:53:27 +0900

    Update ai5.py

--- ai5/ai5.py
+++ ai5/ai5.py
@@ -29,13 +29,17 @@ def extract_article_list(url):
         article_url = urljoin(url, article_link)  # 절대 경로로 변환
         article_links.append(article_url)
 
-    # 5. 기사 본문 추출
+    # 5. 기사 본문, 시간, 태그 추출
     article_contents = []
+    article_times = []
+    article_tags = []
     for link in article_links:
-        article_content = extract_article_content(link)
+        article_content, article_time, article_tag = extract_article_content(link)
         article_contents.append(article_content)
+        article_times.append(article_time)
+        article_tags.append(article_tag)
 
-    return article_titles, article_links, article_contents
+    return article_titles, article_links, article_contents, article_times, article_tags
 
 def extract_article_content(url):
     # 1. URL에서 HTML 내용 가져오기
@@ -52,15 +56,20 @@ def extract_article_content(url):
     article_content_paragraphs = article_content_element.find_all('p')
     article_content = "\n".join([p.get_text() for p in article_content_paragraphs])
 
-    return article_content
+    # 4. 시간 추출
+    article_time = soup.select_one('#article-view > div > header > div > article:nth-child(1) > ul > li:nth-child(2) > i').text
 
+    # 5. 태그 추출
+    article_tag = soup.select_one('#article-view > div > header > nav > ul > li:nth-child(3) > a').text
 
-def save_to_csv(titles, links, contents, filename):
+    return article_content, article_time, article_tag
+
+def save_to_csv(titles, links, contents, times, tags, filename):
     with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(["Title", "Link", "Content"])
-        for title, link, content in zip(titles, links, contents):
-            writer.writerow([title, link, content])
+        writer.writerow(["Time", "Title", "Content", "Link", "Tag"])
+        for time, title, content, link, tag in zip(times, titles, contents, links, tags):
+            writer.writerow([time, title, content, link, tag])
 
 # Streamlit layout
 st.title('WB_ArticleScraper')
@@ -75,9 +84,14 @@ else:
 
 if url:
     try:
-        # Your URL adjustment code here...
-        article_titles, article_links, article_contents = extract_article_list(url)
-        save_to_csv(article_titles, article_links, article_contents, 'articles.csv')
+        # Adjust URL to include https:// if not present
+        if not url.startswith('https://'):
+            if url.startswith('www.'):
+                url = "https://" + url  # 스키마 추가
+            else:
+                url = "https://www." + url  # 스키마 추가
+        article_titles, article_links, article_contents, article_times, article_tags = extract_article_list(url)
+        save_to_csv(article_titles, article_links, article_contents, article_times, article_tags, 'articles.csv')
         st.success('Articles saved to articles.csv')
 
         # Load the CSV data into a pandas DataFrame
@@ -88,3 +102,4 @@ if url:
         
     except Exception as e:
         st.error(f"An error occurred: {e}")
+
