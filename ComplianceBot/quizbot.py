81c1ea15ae8626f5201b88cd6b662fcb83af190d
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-21 09:40:55 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -2,40 +2,31 @@ import streamlit as st
 import pandas as pd
 
 def load_data():
-    data_url = "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/comp_quiz02.csv"
+    data_url = "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/comp_quiz.csv"
     data = pd.read_csv(data_url)
     return data
 
 def main():
-    # 데이터 로드
     data = load_data()
 
-    # Streamlit 앱 구성
     st.title("Quiz App")
     st.write("문항 번호를 입력하여 문제를 풀어보세요.")
 
-    # 시트 이름과 문항 번호 입력
-    sheet_name = st.text_input("시트 이름을 입력하세요.")
     question_number = st.text_input("문항 번호를 입력하세요.")
 
-    if sheet_name and question_number:
-        # 문제 가져오기
-        question = get_question(sheet_name, question_number, data)
+    if question_number:
+        question = data.loc[int(question_number)-65]  # 문항 번호에 해당하는 데이터 가져오기
 
-        if not question.empty:
-            st.write("문제:", question["문항"].values[0])
+        st.write("문제:", question["문항"])
 
-            # OX 버튼 생성
-            user_answer = st.radio("정답을 선택하세요.", ("O", "X"))
+        # O, X 버튼 생성
+        user_answer = st.radio("정답을 선택하세요.", ("O", "X"))
 
-            # 정답 확인
-            if user_answer == question["답안"].values[0]:
-                st.write("정답입니다!")
-            else:
-                st.write("틀렸습니다.")
-                st.write("해설:", question["해설"].values[0])
+        if user_answer == question["답안"]:
+            st.write("정답입니다!")
         else:
-            st.write("문항을 찾을 수 없습니다.")
+            st.write("틀렸습니다.")
+            st.write("해설:", question["해설"])
 
 if __name__ == "__main__":
     main()
