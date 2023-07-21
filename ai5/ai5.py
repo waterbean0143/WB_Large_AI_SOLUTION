26acd29215035521d7fa6ebdea09a7264bc580d0
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-21 16:42:50 +0900

    Update ai5.py

--- ai5/ai5.py
+++ ai5/ai5.py
@@ -1,35 +1,86 @@
 import csv
+import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+from transformers import pipeline
 from urllib.parse import urljoin
 
 def extract_article_list(url):
+    # 1. URL에서 HTML 내용 가져오기
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    html_content = response.content
 
-    article_elements = soup.select('#section-list > ul > li')
+    # 2. HTML 파싱
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-    data = []
-    for article in article_elements:
-        title = article.select_one('div > h4 > a').get('target')
-        link = urljoin(url, article.select_one('div > h4 > a').get('href'))
-        tag = article.select_one('div > span > em:nth-child(1)').text
-        date = article.select_one('div > span > em:nth-child(3)').text
+    # 3. 기사 제목 추출
+    article_titles = []
+    title_elements = soup.select('#section-list > ul > li > h4.titles')
+    for title_element in title_elements:
+        article_title = title_element.get_text()
+        article_titles.append(article_title)
 
-        response = requests.get(link)
-        article_soup = BeautifulSoup(response.content, 'html.parser')
-        content = article_soup.select_one('#snsAnchor > div').text.strip()
+    # 4. 기사 링크 추출
+    article_links = []
+    link_elements = soup.select('#section-list > ul > li > h4 > a')
+    for link_element in link_elements:
+        article_link = link_element.get('href')
+        article_url = urljoin(url, article_link)  # 절대 경로로 변환
+        article_links.append(article_url)
 
-        data.append([date, title, content, link, tag])
+    # 5. 기사 본문 추출
+    article_contents = []
+    for link in article_links:
+        article_content = extract_article_content(link)
+        article_contents.append(article_content)
 
-    return data
+    return article_titles, article_links, article_contents
 
-def save_to_csv(data, filename):
+def extract_article_content(url):
+    # 1. URL에서 HTML 내용 가져오기
+    response = requests.get(url)
+    html_content = response.content
+
+    # 2. HTML 파싱
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # 3. 본문 추출
+    article_content_element = soup.select_one('#snsAnchor > div')
+    if article_content_element is None:
+        raise ValueError("Could not find article content")
+    article_content_paragraphs = article_content_element.find_all('p')
+    article_content = "\n".join([p.get_text() for p in article_content_paragraphs])
+
+    return article_content
+
+def save_to_csv(titles, links, contents, filename):
     with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(['Date', 'Title', 'Content', 'Link', 'Tag'])
-        writer.writerows(data)
+        writer.writerow(["Title", "Link", "Content"])
+        for title, link, content in zip(titles, links, contents):
+            writer.writerow([title, link, content])
 
-url = 'https://www.aitimes.kr/news/articleList.html?page=2&total=3388&sc_section_code=S1N4&sc_sub_section_code=&sc_serial_code=&sc_area=&sc_level=&sc_article_type=&sc_view_level=&sc_sdate=&sc_edate=&sc_serial_number=&sc_word=&box_idxno=&sc_multi_code=&sc_is_image=&sc_is_movie=&sc_user_name=&sc_order_by=E&view_type=sm'
-data = extract_article_list(url)
-save_to_csv(data, 'articles.csv')
+# Streamlit layout
+st.title('WB_ArticleScraper')
+
+# URL 선택 옵션
+option = st.selectbox('URL 입력 방식', ['인공지능신문(aitimes) AI 산업군 - 제목형', '직접 입력'])
+
+if option == '인공지능신문(aitimes) AI 산업군 - 제목형':
+    url = "https://www.aitimes.kr/news/articleList.html?page=1&total=3382&sc_section_code=S1N4&sc_sub_section_code=&sc_serial_code=&sc_area=&sc_level=&sc_article_type=&sc_view_level=&sc_sdate=&sc_edate=&sc_serial_number=&sc_word=&box_idxno=&sc_multi_code=&sc_is_image=&sc_is_movie=&sc_user_name=&sc_order_by=E"
+else:
+    url = st.text_input("뉴스 기사 리스트 URL을 입력하세요: ", "")
+
+if url:
+    try:
+        # Adjust URL to include https:// if not present
+        if not url.startswith('https://'):
+            if url.startswith('www.'):
+                url = "https://" + url  # 스키마 추가
+            else:
+                url = "https://www." + url  # 스키마 추가
+        article_titles, article_links, article_contents = extract_article_list(url)
+        save_to_csv(article_titles, article_links, article_contents, 'articles.csv')
+        st.success('Articles saved to articles.csv')
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
