0a878a8e51b2861dcb8d2c698bd3198b824aa499
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-21 10:00:19 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -10,26 +10,32 @@ def main():
     data = load_data()
 
     st.title("Quiz App")
-    st.write("문항 번호를 입력하여 문제를 풀어보세요.")
+    st.write("문항을 풀어보세요.")
 
-    question_number = st.text_input("문항 번호를 입력하세요.")
+    total_questions = len(data)  # 전체 문항 수
+    correct_count = 0  # 정답 개수
+    wrong_questions = []  # 틀린 문항 번호
 
-    if question_number:
-        question = data.loc[int(question_number)-65]  # 문항 번호에 해당하는 데이터 가져오기
-
-        st.write("문제:", question["문항"])
+    for i, question in data.iterrows():
+        st.write(f"문제 {i+1}/{total_questions}:")
+        st.write("문항:", question["문항"])
 
         # O, X 버튼 생성
-        user_answer = st.radio("정답을 선택하세요.", (None, "O", "X"))
+        user_answer = st.radio("정답을 선택하세요.", ("O", "X"))
 
-        if user_answer is None:
-            st.write("O나 X를 선택한 후 문제를 풀어주세요.")
+        if user_answer == question["답안"]:
+            st.write("정답입니다!")
+            correct_count += 1
         else:
-            if user_answer == question["답안"]:
-                st.write("정답입니다!")
-            else:
-                st.write("틀렸습니다.")
-                st.write("해설:", question["해설"])
+            st.write("틀렸습니다.")
+            st.write("해설:", question["해설"])
+            wrong_questions.append(i+65)  # 틀린 문항 번호 저장
+
+        st.write("---")
+
+    st.write("모든 문항을 푸셨습니다!")
+    st.write("정답 개수:", correct_count)
+    st.write("틀린 문항 번호:", wrong_questions)
 
 if __name__ == "__main__":
     main()
