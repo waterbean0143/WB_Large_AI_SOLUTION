5dae614ac141e22fc925c7db4fdebf8567235976
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-21 10:06:18 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -8,37 +8,40 @@ def load_data():
 
 def main():
     data = load_data()
+    total_questions = len(data)  # 전체 문항 수
 
     st.title("Quiz App")
     st.write("문항을 풀어보세요.")
 
-    total_questions = len(data)  # 전체 문항 수
-    correct_count = 0  # 정답 개수
-    wrong_questions = []  # 틀린 문항 번호
+    question_number = st.empty()  # 문항 번호 표시할 빈 컨테이너
+    question_text = st.empty()  # 문항 텍스트 표시할 빈 컨테이너
+    user_answer = st.empty()  # 사용자 정답 선택할 빈 컨테이너
+    feedback_message = st.empty()  # 정답 여부 피드백 표시할 빈 컨테이너
 
-    for i, question in data.iterrows():
-        st.write(f"문제 {i+1}/{total_questions}:")
-        st.write("문항:", question["문항"])
+    current_question = 0  # 현재 문항 인덱스
 
-        # O, X 버튼 생성
-        user_answer = st.radio("정답을 선택하세요.", (None, "O", "X"))
+    while current_question < total_questions:
+        question_number.write(f"문제 {current_question+1}/{total_questions}:")
+        question_text.write("문항:", data.loc[current_question, "문항"])
 
-        if user_answer is None:
-            st.write("O나 X를 선택한 후 문제를 풀어주세요.")
-        else:
-            if user_answer == question["답안"]:
-                st.write("정답입니다!")
-                correct_count += 1
+        if user_answer.button("제출"):  # 제출 버튼 누르면 정답 체크
+            if user_answer.radio("정답을 선택하세요.", ("O", "X")) == data.loc[current_question, "답안"]:
+                feedback_message.write("정답입니다!")
             else:
-                st.write("틀렸습니다.")
-                st.write("해설:", question["해설"])
-                wrong_questions.append(i+65)  # 틀린 문항 번호 저장
+                feedback_message.write("틀렸습니다.")
+                st.write("해설:", data.loc[current_question, "해설"])
 
-        st.write("---")
+            current_question += 1  # 다음 문항으로 이동
 
-    st.write("모든 문항을 푸셨습니다!")
-    st.write("정답 개수:", correct_count)
-    st.write("틀린 문항 번호:", wrong_questions)
+            # 다음 문항으로 이동하기 위한 버튼
+            if current_question < total_questions:
+                if st.button("다음 문제"):
+                    question_number.empty()
+                    question_text.empty()
+                    user_answer.empty()
+                    feedback_message.empty()
+            else:
+                st.write("모든 문항을 푸셨습니다!")
 
 if __name__ == "__main__":
     main()
