4f8a58a19cfc5a890fc8ed5b07be1c2d361e846d
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-25 14:32:55 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -10,57 +10,56 @@ def main():
     st.write("문항을 풀어보세요.")
 
     files = {
-        "2. 청탁금지법": "quiz02.csv",
-        "3. 힘/성희롱": "quiz03.csv",
-        "4. 회계세무": "quiz04.csv",
-        "5. 담합예방": "quiz05.csv",
-        "6. 제3위험평가": "quiz06.csv",
-        "7. 안전보건": "quiz07.csv",
-        "8. 부당특약": "quiz08.csv",
-        "9. 재원집행(대가지급)": "quiz09.csv",
-        "12. 기술자료 탈취": "quiz12.csv",
-        "14. 위장도급": "quiz14.csv",
-        "16. 제안센터 정보보안": "quiz16.csv"
+        "quiz02.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz02.csv",
+        "quiz03.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz03.csv",
+        "quiz04.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz04.csv",
+        "quiz05.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz05.csv",
+        "quiz06.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz06.csv",
+        "quiz07.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz07.csv",
+        "quiz08.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz08.csv",
+        "quiz09.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz09.csv",
+        "quiz12.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz12.csv",
+        "quiz14.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz14.csv",
+        "quiz16.csv": "https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/quiz16.csv"
     }
 
-    selected_text = st.selectbox("문항을 선택하세요.", list(files.keys()))
+    selected_file = st.selectbox("CSV 파일 선택", list(files.keys()))
 
-    if selected_text:
-        selected_file = files[selected_text]
-        file_url = f"https://github.com/waterbean0143/WB_Large_AI_SOLUTION/raw/main/ComplianceBot/{selected_file}"
+    if selected_file:
+        file_url = files[selected_file]
         data = load_data(file_url)
 
-        total_questions = len(data)
-        user_answers = [None] * total_questions
+        total_questions = len(data)  # 전체 문항 수
+        user_answers = [None] * total_questions  # 사용자 답변 리스트 초기화
 
-        form = st.form("quiz_form")
-        answer_labels = []
+        with st.form("quiz_form"):
+            for i in range(total_questions):
+                st.write(f"문제 {i+1}/{total_questions}:")
+                st.write("문항:", data.loc[i, "문항"])
+                user_answers[i] = st.radio("정답을 선택하세요.", options=["O", "X"], key=f"answer_{i}")
 
-        for i in range(total_questions):
-            st.write(f"문제 {i+1}/{total_questions}:")
-            st.write("문항:", data.loc[i, "문항"])
-            user_answers[i] = form.radio("정답을 선택하세요.", options=["O", "X"], key=f"answer_{i}")
-            answer_labels.append(form.empty())  # 추후 해설을 추가할 라벨 저장
+                # Create empty explanations for each question
+                explanation = st.empty()
 
-        submitted = form.form_submit_button("제출")
-
-        if submitted:
+        # Display the explanation after the user clicks the "제출" (Submit) button
+        if st.form_submit_button("제출"):
+            st.write(f"총 {total_questions}문제 중")
             correct_count = 0
-            st.markdown(f"총 {total_questions}문제 중")
             incorrect_questions = []
-
             for i in range(total_questions):
                 if user_answers[i] == data.loc[i, "답안"]:
                     correct_count += 1
                 else:
                     incorrect_questions.append(i)
-                    # 틀린 문항 아래에 해설 추가
-                    answer_labels[i].write("해설:", data.loc[i, "해설"])
 
-            st.markdown(f"**- {correct_count}문제 맞추셨습니다!**")
+            st.write(f"{correct_count}문제 맞추셨습니다!")
 
-            if len(incorrect_questions) > 0:
-                st.markdown("**틀린 문제와 해설:**")
+            if incorrect_questions:
+                st.write("틀린 문제:")
+                for question_num in incorrect_questions:
+                    st.write(f"문제 {question_num+1}:")
+                    st.write("문항:", data.loc[question_num, "문항"])
+                    explanation.text("틀린 이유: " + data.loc[question_num, "해설"])
 
 if __name__ == "__main__":
     main()
