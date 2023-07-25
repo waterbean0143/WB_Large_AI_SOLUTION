acf67d53944936b6b7062ab67727301751384de0
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-25 14:37:04 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -32,9 +32,8 @@ def main():
         total_questions = len(data)  # 전체 문항 수
         user_answers = [None] * total_questions  # 사용자 답변 리스트 초기화
 
-        with st.sidebar:  # Add content to the sidebar
-            incorrect_questions_list = st.empty()
-            explanations_list = st.empty()
+        incorrect_questions = []
+        explanations = []
 
         with st.form("quiz_form"):
             for i in range(total_questions):
@@ -42,25 +41,23 @@ def main():
                 st.write("문항:", data.loc[i, "문항"])
                 user_answers[i] = st.radio("정답을 선택하세요.", options=["O", "X"], key=f"answer_{i}")
 
-        # Display the explanation after the user clicks the "제출" (Submit) button
-        if st.form_submit_button("제출"):
-            incorrect_questions = []
-            explanations = []
-
-            for i in range(total_questions):
                 if user_answers[i] != data.loc[i, "답안"]:
                     incorrect_questions.append(f"문제 {i+1}")
                     explanations.append(data.loc[i, "해설"])
 
-            # Update the sidebar with incorrect questions and explanations
-            if incorrect_questions:
-                incorrect_questions_list.header("틀린 문제:")
-                incorrect_questions_list.write("\n".join(incorrect_questions))
+            submitted = st.form_submit_button("제출")  # Move submit button inside the form context
 
-                explanations_list.header("해설:")
-                explanations_list.write("\n".join(explanations))
-            else:
-                incorrect_questions_list.write("모든 문제를 정답으로 맞추셨습니다!")
+        # Display the explanation after the user clicks the "제출" (Submit) button
+        if submitted:
+            with st.sidebar:  # Add content to the right sidebar
+                if incorrect_questions:
+                    st.header("틀린 문제:")
+                    st.write("\n".join(incorrect_questions))
+
+                    st.header("해설:")
+                    st.write("\n".join(explanations))
+                else:
+                    st.write("모든 문제를 정답으로 맞추셨습니다!")
 
 if __name__ == "__main__":
     main()
