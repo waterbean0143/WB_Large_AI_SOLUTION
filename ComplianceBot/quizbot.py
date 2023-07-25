b1d8d4a91c8abb1dda0c1f5e11d5fab400e27bf1
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2023-07-25 15:03:20 +0900

    Update quizbot.py

--- ComplianceBot/quizbot.py
+++ ComplianceBot/quizbot.py
@@ -5,7 +5,24 @@ def load_data(file):
     data = pd.read_csv(file)
     return data
 
+def noCopyPaste():
+    # Custom CSS styles to disable text selection and copying
+    custom_css = '''
+    <style>
+    body {
+        user-select: none;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+    }
+    </style>
+    '''
+
+    # Render the custom CSS styles
+    st.markdown(custom_css, unsafe_allow_html=True)
+
 def main():
+    noCopyPaste()  # Call the noCopyPaste function to disable text selection and copying
+
     st.title("Quiz App")
     st.write("문항을 풀어보세요.")
 
@@ -30,44 +47,39 @@ def main():
         data = load_data(file_url)
 
         total_questions = len(data)  # 전체 문항 수
-        user_answers = ["미선택"] * total_questions  # 사용자 답변 리스트 초기화
-        unanswered_questions = []  # List to store question numbers with "[미선택]" option checked
-
-        incorrect_questions = []
-        explanations = []
+        user_answers = [None] * total_questions  # 사용자 답변 리스트 초기화
 
         with st.form("quiz_form"):
             for i in range(total_questions):
                 st.write(f"문제 {i+1}/{total_questions}:")
                 st.write("문항:", data.loc[i, "문항"])
                 user_answers[i] = st.radio("정답을 선택하세요.", options=["미선택", "O", "X"], key=f"answer_{i}")
-                st.write("")  # Add an empty line for spacing
 
-                if user_answers[i] == "미선택":
-                    unanswered_questions.append(i+1)
-                elif user_answers[i] != data.loc[i, "답안"]:
-                    incorrect_questions.append(i+1)
-                    explanations.append(data.loc[i, "해설"])
+            submitted = st.form_submit_button("제출")
 
-            submitted = st.form_submit_button("제출")  # Move submit button inside the form context
-
-        # Display the explanation after the user clicks the "제출" (Submit) button
         if submitted:
-            with st.sidebar:  # Add content to the right sidebar
-                if incorrect_questions:
-                    st.header("틀린 문제:")
-                    st.write(", ".join([f"문제 {q}번" for q in incorrect_questions]))
+            st.write(f"총 {total_questions}문제 중")
+            correct_count = 0
+            incorrect_questions = []
+            for i in range(total_questions):
+                if user_answers[i] == "미선택":
+                    incorrect_questions.append(i+1)
+                elif user_answers[i] == data.loc[i, "답안"]:
+                    correct_count += 1
+                else:
+                    incorrect_questions.append(i+1)
 
-                    st.header("해설:")
-                    for i, explanation in enumerate(explanations):
-                        st.write(f"[{incorrect_questions[i]}번 해설] : {explanation}")
+            if len(incorrect_questions) == 0:
+                st.write("정답을 모두 맞추셨습니다!")
+            else:
+                st.write(f"[미선택]이 체크되어있는 문제 {', '.join(map(str, incorrect_questions))}번을 안풀었습니다.")
 
-                if unanswered_questions:
-                    st.warning("[미선택] 항목을 체크한 문제:")
-                    st.write(", ".join([f"문제 {q}번" for q in unanswered_questions]))
-
-                if not incorrect_questions and not unanswered_questions:
-                    st.write("모든 문제를 정답으로 맞추셨습니다!")
+            if len(incorrect_questions) > 0:
+                st.write("틀린 문제 및 해설:")
+                for question_num in incorrect_questions:
+                    st.write(f"문제 {question_num}:")
+                    st.write("문항:", data.loc[question_num-1, "문항"])
+                    st.write(f"[{question_num}번 해설]:", data.loc[question_num-1, "해설"])
 
 if __name__ == "__main__":
     main()
